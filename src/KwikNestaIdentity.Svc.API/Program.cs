3a2df51e7a7a45b1b7db9788f8d048b33934eb5c
Toba Ojo <ojotobar@gmail.com>
2025-10-01 12:31:19 +0100

    Added Kestrel configurations and updated dockerfile

--- src/KwikNestaIdentity.Svc.API/Program.cs
+++ src/KwikNestaIdentity.Svc.API/Program.cs
@@ -10,7 +10,7 @@ SerilogBootstrapper.UseBootstrapLogger();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.WebHost.ConfigureKestrel();
 //builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
